bd5e1053d0e5419dc566654f6bcc4bb9e15307fa
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-30 16:16:55 -0500

    solves problem 3 homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movies.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Movie whatMovie='Squel Magnolias' />
+      <Movie whatMovie='Everything Meryl Streep' />
+      <Movie whatMovie='Chicago' />
     </div>
   );
 }
---
+++ src/Movies.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Movie ({whatMovie}) {
+    return (
+        <h1>Movie Name: {whatMovie}</h1>
+    )
+};
+
+export default Movie;
